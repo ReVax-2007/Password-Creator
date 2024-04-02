4032ca051cc4e04f14eb11ac3bf65ce0ff226888
Xavier Voet <voet.xavier@gmail.com>
2024-04-02 15:01:59 -0400

    smal update made

--- password.py
+++ password.py
@@ -1,45 +1,49 @@
 import tkinter as tk
 
 # VARIABLES
-
 CharacterCount = " "
 Characters = " "
-On_switch = " "
-On_switch2 = " "
+On_switch = False
+On_switch2 = False
 
 # LOGIC code
-
 def show_values():
     global Characters
-    print(w.get())
     Characters = str(w.get())
     print("Characters:", Characters)
-    return Characters
 
-def toggle_switch():
+def toggle_switch(*args):
     global On_switch
     if toggle_var.get() == 1:
         print("Toggle switched on")
-        # Add your code to record data when switch is on
         On_switch = True
-    elif toggle_var.get() == 2:
+    else:
         print("Toggle switched off")
-        # Add your code to record data when switch is off
         On_switch = False
+    Check_toggle()
 
-def toggle_switch2():
+def toggle_switch2(*args):
     global On_switch2
     if toggle_var2.get() == 1:
         print("Toggle2 switched on")
-        # Add your code to record data when switch is on
         On_switch2 = True
-    elif toggle_var2.get() == 2:
+    else:
         print("Toggle2 switched off")
-        # Add your code to record data when switch is off
         On_switch2 = False
+    Check_toggle()
+
+def Check_toggle():
+    if On_switch:
+        print("Switch 1 is on")
+    else:
+        print("Switch 1 is off")
+    
+    if On_switch2:
+        print("Switch 2 is on")
+    else:
+        print("Switch 2 is off")
 
 # UI code
-    
 root = tk.Tk()
 root.geometry("1000x600")
 root.title("Password Generator")
@@ -56,19 +60,15 @@ Button1 = tk.Button(root, text='Show', command=show_values)
 Button1.pack()
 
 toggle_var = tk.IntVar()
-toggle_button = tk.Checkbutton(root, text="Toggle 1", variable=toggle_var, command=toggle_switch)
+toggle_var.trace('w', toggle_switch)  # Tracing changes to toggle_var
+toggle_button = tk.Checkbutton(root, text="Toggle 1", variable=toggle_var)
 toggle_button.pack(padx=10, pady=10)
 
 toggle_var2 = tk.IntVar()
-toggle_button2 = tk.Checkbutton(root, text="Toggle 2", variable=toggle_var2, command=toggle_switch2)
+toggle_var2.trace('w', toggle_switch2)  # Tracing changes to toggle_var2
+toggle_button2 = tk.Checkbutton(root, text="Toggle 2", variable=toggle_var2)
 toggle_button2.pack(padx=10, pady=10)
 
-def Check_toggle():
-    if On_switch == True:
-        print("switch is on")
-    else:
-        print("switch isnt on")
-
-Check_toggle()
+Check_toggle()  # Initial check of toggle switches
 
 root.mainloop()
